7c6c349b048e0a05eeada33cb58555a89d2e943d
maryam-develop <maryamdev.lop@gmail.com>
2023-05-13 22:04:19 +0500

    FEAT: assignment 4 complete

--- Assignments/Assignment-3/app.js
+++ Assignments/Assignment-3/app.js
@@ -20,11 +20,11 @@
 // c. Quantity i.e. how many products a visitor wants to order
 // Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
 
-var visitorName = "John Doe";
-var Product = "T-shirt";
-var quantity = 5;
+// var visitorName = "John Doe";
+// var Product = "T-shirt";
+// var quantity = 5;
 
-document.write("<b>" + visitorName +"</b>" +" ordered " +"<b>" + quantity +"-"+Product +"</b>"+ " on XYZ Clothing store");
+// document.write("<b>" + visitorName +"</b>" +" ordered " +"<b>" + quantity +"-"+Product +"</b>"+ " on XYZ Clothing store");
  
 
 
---
+++ Assignments/Assignment-4/app.js
@@ -0,0 +1,35 @@
+// 1. Declare 3 variables in one statement.
+// var str ;
+// var str01;
+// var num;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// legal variable
+// 1 var mak_food;
+// 2 var myNum;
+// 3 var my1name;
+// 4 var favFoodIsBiryani;
+// 5 var call$
+
+// illegal variable
+
+// 1 var Marks always;
+// 2 var #myNum;
+// 3 var 1name;
+// 4 var iahjdaoisrjooweoeofeojidhuhow;
+// 5 var var;
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+document.write("<h1>Rules for naming JS variables</h1> <br>" +"Variable names can only contain numbers, $ and _. For Example: $name, _name or name<br>"+" Variable names are case Sensitive <br>"+"Variable names should not be JS")
+
+
+
