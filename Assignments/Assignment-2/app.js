1ac3baedc1495519ea0010b329784425f94e90f5
maryam-develop <maryamdev.lop@gmail.com>
2023-05-13 10:45:20 +0500

    FEAT: Q no 1 to 6

--- Assignments/Assignment-2/app.js
+++ Assignments/Assignment-2/app.js
@@ -1,1 +1,44 @@
-alert("hello")
+// 1. Declare a variable called username.
+// var username 
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// var myName = "Maryam Anwer";
+// alert(myName)
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+
+// var message ;
+
+// b) Assign “Hello World” to variable message
+
+// message = "Hello World";
+
+// c) Display the message in alert box.
+
+// alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var studentCourse = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentCourse);
+
+
+// 5. Write a script to display the following alert using one JS variable:
+
+// var food = "pizza \npizz \npiz \npi \np";
+
+// alert(food);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email = "myheadache745@gmail.com";
+alert("My Email Address is "+email);
