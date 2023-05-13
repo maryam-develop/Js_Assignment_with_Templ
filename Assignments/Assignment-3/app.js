80756e5b5b0e4c9fc0eb6658eb2ba3871e762bc6
maryam-develop <maryamdev.lop@gmail.com>
2023-05-13 13:32:20 +0500

    FEAT : qno4

--- Assignments/Assignment-3/app.js
+++ Assignments/Assignment-3/app.js
@@ -14,6 +14,15 @@
 // var birthYear = 2001 ;
 // document.write("My birth year is "+ birthYear + "<br>"  + "Data type of my declare variable is number");
 
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
 
+var visitorName = "John Doe";
+var Product = "T-shirt";
+var quantity = 5;
 
-
+document.write("<b>" + visitorName +"</b>" +"ordered " +"<b>" + quantity +"-"+Product +"</b>"+ " on XYZ Clothing store");
+ 
