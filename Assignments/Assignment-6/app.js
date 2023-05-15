eac221f50684fd9b000994b493268bb00d4ff1bf
maryam-develop <maryamdev.lop@gmail.com>
2023-05-16 01:03:56 +0500

    "FEAT : qno1"

---
+++ Assignments/Assignment-6/app.js
@@ -0,0 +1,16 @@
+// 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+
+// var num = 8;
+// document.write("<h3>Result</h3><br>The vale of a is "+ num+"<br> ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,<br><br>");
+//  num = ++num;
+// document.write("The value of is ++a: "+ num + "<br>now The value of is a: "+ num)
+// num = num ++ ;
+// document.write("<br><br>The value of is a++: "+ num + "<br>now The value of is a: "+ num)
+// num = --num;
+// document.write("<br><br>The value of is --a: "+ num + "<br>now The value of is a: "+ num)
+// num = num--;
+// document.write("<br><br>The value of is a--: "+ num + "<br>now The value of is a: "+ num--)
+
+ 
