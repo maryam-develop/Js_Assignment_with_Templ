e9a5da54f88090e1de3bca1856e9772d318003ee
maryam-develop <maryamdev.lop@gmail.com>
2023-05-16 07:30:20 +0500

    FEAT: qno 2

--- Assignments/Assignment-6/app.js
+++ Assignments/Assignment-6/app.js
@@ -13,4 +13,30 @@
 // num = num--;
 // document.write("<br><br>The value of is a--: "+ num + "<br>now The value of is a: "+ num--)
 
- 
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+
+
+
+// var a = 2, b = 1;
+// document.write("a is "+ a+ "<br>b is "+ b)
+
+// var result = --a ;
+// document.write("<br>resul of --a ="+result)
+// result = --a - --b;
+// document.write("<br>resul of --a - --b =  "+result)
+// result= --a - --b + ++b;
+// document.write("<br>resul of --a - --b + ++b = "+result)
+// result= --a - --b + ++b + b--;
+
+// document.write("<br>resul of --a - --b + ++b + b-- = "+result)
+
+
+
+
+
