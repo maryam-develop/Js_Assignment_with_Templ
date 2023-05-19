4bc57e00cc0e71c35c3cf11496cb33dbbad8fd23
maryam-develop <maryamdev.lop@gmail.com>
2023-05-20 01:02:25 +0500

    FEAT: add assignment-7 qno 1

---
+++ Assignments/Assignment-7/app.js
@@ -0,0 +1,9 @@
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// var userInput= prompt("Enter your city name ?");
+// if (userInput=="Karachi"||"karachi"){
+//     alert("Welcome to city of lights");
+// }
+
