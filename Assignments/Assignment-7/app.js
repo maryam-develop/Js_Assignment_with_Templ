3257ade5de698bdd208654177770680f460931d1
maryam-develop <maryamdev.lop@gmail.com>
2023-05-20 01:35:51 +0500

    FEAT: qno 3 and 4

--- Assignments/Assignment-7/app.js
+++ Assignments/Assignment-7/app.js
@@ -7,3 +7,36 @@
 //     alert("Welcome to city of lights");
 // }
 
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt("write your gender");
+
+// if (gender=="female"||"Female"){
+//     alert("Good Morning Sir");
+//     }
+//     else if (gender=="male"||"Male"){
+//         alert("Good Morning Ma’am");
+//         }
+//         else {
+//             alert("please write male or female");
+//             }
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+var inputColor = prompt("Color of road traffic signal");
+if (inputColor=="red"||"Red"){
+    alert("Must Stop");
+    }else
+     if (inputColor=="yellow"||"Yellow"){
+        alert("Ready to Move");
+    }else if (inputColor=="green"||"Green") {
+        alert("Move now");
+
+    }else {
+        alert("please write red, yellow or green");
+    }
+
+
