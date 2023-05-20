496ede3142968cb968a4c9b9f663687bbea8d75b
maryam-develop <maryamdev.lop@gmail.com>
2023-05-20 15:11:46 +0500

    FEAT: qno 6

--- Assignments/Assignment-8/app.js
+++ Assignments/Assignment-8/app.js
@@ -74,3 +74,21 @@
 // } else {
 //     console.log("Incorrect password");
 // }  
+// 6. This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+// }
+
+var greeting;
+var hour = 13;
+if (hour < 18) {
+greeting = "Good day";
+alert(greeting);
+}else{
+greeting = "Good evening";
+alert(greeting);
+}
