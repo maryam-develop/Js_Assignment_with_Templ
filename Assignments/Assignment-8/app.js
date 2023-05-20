692862bd02c4e3865a2a11b3c85c1f1f6d80360a
maryam-develop <maryamdev.lop@gmail.com>
2023-05-20 14:34:51 +0500

    FEAT: qno1

---
+++ Assignments/Assignment-8/app.js
@@ -0,0 +1,15 @@
+// 1. Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// codes:- A=65, Z=90, a=97, z=122).
+
+// var userInput = prompt("enter a character(number or string)")
+// if(userInput >= 65 && userInput <=90 || userInput >= "A" && userInput <= "Z" ){
+// console.log("uppercase letter")
+// }else if (userInput >= 97 && userInput <=122 || userInput >= "a" && userInput <= "z" ){
+//     console.log("lower case letter")
+// }else{
+//     console.log("Numbers")  
+
+// }
+
