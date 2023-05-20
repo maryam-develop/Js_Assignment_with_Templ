3a4102fffb16f68624933aa8a251f69c344f632b
maryam-develop <maryamdev.lop@gmail.com>
2023-05-20 20:43:05 +0500

    FEAT: qno1 to 6

---
+++ Assignments/Assignment-9/app.js
@@ -0,0 +1,17 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// var stdName = [];
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+// var stdName = [];
+
+// 3. Declare and initialize a strings array.
+// var stdNam = ['John', 'Mary', 'Peter', 'Paul', 'Mary'];
+// 4. Declare and initialize a number array.
+// var stdNum = [1,2, 3, 4, 5, 6];
+// 5. Declare and initialize a boolean array.
+// var stdBool = [true, false];
+// 6. Declare and initialize a mixed array.
+// var stdMix = ['John', 1, true, 'Mary', 2, false];
