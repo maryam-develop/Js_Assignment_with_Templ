40e10670227518e5616cab40ed8d443be5e7e53a
maryam-develop <maryamdev.lop@gmail.com>
2023-05-21 02:36:28 +0500

    FEAT: qno7

--- Assignments/Assignment-9/app.js
+++ Assignments/Assignment-9/app.js
@@ -15,3 +15,23 @@
 // var stdBool = [true, false];
 // 6. Declare and initialize a mixed array.
 // var stdMix = ['John', 1, true, 'Mary', 2, false];
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like
+
+// var eduPak = []
+// var ed1 = "1) SSC";
+// var ed2 = "2) HSC";
+// var ed3 = "3) BCS";
+// var ed4 = "4) BS";
+// var ed5 = "5) BCOM";
+// var ed6 = "6) MS";
+// var ed7 = "7) M. Phil.";
+// var ed8 = "8) PhD";
+
+// document.write("<h3>Qualificatin</h3><br>"+ ed1 +"<br>"+ed2+"<br>"+ed3+"<br>"+ed4+"<br>"+ed5+"<br>"+ed6+"<br>"+ed7+"<br>"+ed8 )
+
+
+
