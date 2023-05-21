a3be875416cdf843b37c8fa2ee436541b27026dd
maryam-develop <maryamdev.lop@gmail.com>
2023-05-21 05:20:18 +0500

    FEAT: qno 09

--- Assignments/Assignment-9/app.js
+++ Assignments/Assignment-9/app.js
@@ -42,3 +42,50 @@
 // document.write("Score of " +stdNam[0]+" is" +stdSco[0]+". Percentage :"+ stdPer[0]+"%")
 // document.write("<br>Score of " +stdNam[1]+" is" +stdSco[1]+". Percentage :"+ stdPer[1]+"%")
 // document.write("<br>Score of " +stdNam[2]+" is" +stdSco[2]+". Percentage :"+ stdPer[2]+"%")
+
+// 9. Initialize an array with color names. Display the array elements in your browser.
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array. Display the updated array in your browser.
+
+// var colorlist = ["red", "blue","pink","yellow","black"];
+// var userColor = prompt("what color you want in begining"+"\n red, blue,pink,yellow,black");
+
+// colorlist.unshift(userColor);
+// document.write("<h3>Color List</h3><br>"+ colorlist )
+
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display the updated array in your browser.
+
+// var userColor = prompt("what color you want in last"+"\n red, blue,pink,yellow,black");
+
+// colorlist.push(userColor);
+// document.write("<h3>Color List</h3><br>"+ colorlist )
+// c. Add two more color to the beginning of the array.Display the updated array in your browser.
+// colorlist.push("orange", "indigo");
+// document.write("<h3>Color List</h3><br>"+ colorlist )
+
+// d. Delete the first color in the array. Display the updated array in your browser.
+
+// colorlist.shift();
+// document.write("<h3>Color List</h3><br>"+ colorlist );
+
+// e. Delete the last color in the array. Display the updated array in your browser.
+
+// colorlist.pop();
+// document.write("<h3>Color List</h3><br>"+ colorlist );
+
+// f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. . Display the updated array in your browser
+
+// var userColor = prompt("which index you want to color add");
+// var addColor = prompt("add color name ");
+
+// colorlist.splice(userColor,0 , addColor);
+
+// document.write("<h3>Color List</h3><br>"+ colorlist );
+
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index. . Display the updated array in your browser.
+// var delColor = prompt("which index you want to color delete");
+// var delColorNum = prompt("hoe many color you want to delete");
+// colorlist.splice(delColor,delColorNum);
+// document.write("<h3>Color List</h3><br>"+ colorlist );
+
+
+
