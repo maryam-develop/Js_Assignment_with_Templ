7a338364ced33e868fe2bd3ea99bc51417b68c27
maryam-develop <maryamdev.lop@gmail.com>
2023-05-22 06:39:25 +0500

    FEAT: qno 4

--- Assignments/Assignment-10/app.js
+++ Assignments/Assignment-10/app.js
@@ -14,11 +14,18 @@
 // for (var index = 1; index <= 10; index++) {
 //      console.log(index);
     
-}
+//}
 
+// 4. Write a program to print multiplication table of any number using for loop. Table number & length should be taken as an input from user
+ 
+var userNum = +prompt("Enter a number to show its multiplications table");
+ var userleng = +prompt("Enter length multiplications table");
+document.write("Multiplication table of "+ userNum+ " Length of " +userleng +"<br>");
 
+ for (var i = 1; i<= userleng; i++ ){
+document.write(userNum + " * " + i + " = " +  userNum*i +"<br>" )
 
-
+ }
 
 
 
