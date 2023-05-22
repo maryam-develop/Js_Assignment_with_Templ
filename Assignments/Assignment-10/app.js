e68f02d53fc4f3766db627910bec08134f70054e
maryam-develop <maryamdev.lop@gmail.com>
2023-05-22 05:39:16 +0500

    FEAT:qno 1 &2

---
+++ Assignments/Assignment-10/app.js
@@ -0,0 +1,27 @@
+// 1. Declare and initialize an empty multidimensional array. (Array of arrays)
+
+// var arr = [];
+
+// 2. Declare and initialize a multidimensional array representing the following matrix:
+
+// arr[0]=[0,1,2,3];
+// arr[1]=[1,0,1,2];
+// arr[2]=[2,1,0,1];
+// document.write(arr[0]+"<br>"+arr[1]+"<br>"+arr[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
