33114bb2c0da146eca8b130288d6616e128271f9
maryam-develop <maryamdev.lop@gmail.com>
2023-05-22 05:42:46 +0500

    FEAT: qno4

--- Assignments/Assignment-10/app.js
+++ Assignments/Assignment-10/app.js
@@ -9,8 +9,12 @@
 // arr[2]=[2,1,0,1];
 // document.write(arr[0]+"<br>"+arr[1]+"<br>"+arr[2]);
 
+// 3 Write a program to print numeric counting from 1 to 10.
 
-
+// for (var index = 1; index <= 10; index++) {
+//      console.log(index);
+    
+}
 
 
 
