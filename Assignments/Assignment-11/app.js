e78d3e6eb8dc73a038c45a9bdcfe31e9e862b62d
maryam-develop <maryamdev.lop@gmail.com>
2023-05-27 00:04:19 +0500

    FEAT: qno 16

--- Assignments/Assignment-11/app.js
+++ Assignments/Assignment-11/app.js
@@ -99,3 +99,15 @@
 //          document.write("We are Sorry. "+userInput+ " is <b>not available</b> in our bakery"); }
 
 
+// 16. Write a program to convert the following string to an
+// array using string split method.
+// var university = “University of Karachi”;
+// Display the elements of array in your browser
+// var university = "University of Karachi";
+// var arr = university.split("");
+// for (var i = 0; i < arr.length; i++) {
+//     document.write(arr[i]+"<br>");
+//     }
+
+
+
