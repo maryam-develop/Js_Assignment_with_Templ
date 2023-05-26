3da2421382c5565a70ac78e84bac767a8301b401
maryam-develop <maryamdev.lop@gmail.com>
2023-05-26 23:56:22 +0500

    FEAT:check

--- Assignments/Assignment-11/app.js
+++ Assignments/Assignment-11/app.js
@@ -98,16 +98,4 @@
 // if (matchfound === false) {
 //          document.write("We are Sorry. "+userInput+ " is <b>not available</b> in our bakery"); }
 
-// 15. Write a program to take password as an input from
-// user. The password must qualify these requirements:
-// a. It should contain alphabets and numbers
-// b. It should not start with a number
-// c. It must at least 6 characters long
-// If the password does not meet above requirements,
-// prompt the user to enter a valid password.
-// For character codes of a-z, A-Z & 0-9, refer to ASCII
-// table at the end of this document.
-
-
-
 
