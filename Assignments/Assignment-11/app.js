ee3d8cc6735c5e5093bfc768013b97e4b74077f9
maryam-develop <maryamdev.lop@gmail.com>
2023-05-26 21:10:04 +0500

    FEAT: qno 7

--- Assignments/Assignment-11/app.js
+++ Assignments/Assignment-11/app.js
@@ -26,3 +26,7 @@
 
 // document.write("Hi "+ firstName.concat(" "+lastName) );
 
+// 7. Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser.
+
+// var Name= "Hyderabad";
+// document.write("City: "+Name+"<br>After replacement: "+Name.replace("Hyder","Islam"))
