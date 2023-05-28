a5af03f8b11498020171b302b2b1866a25886cce
maryam-develop <maryamdev.lop@gmail.com>
2023-05-28 14:53:44 +0500

    FEAT: qno 2

--- Assignments/Assignment-12/app.js
+++ Assignments/Assignment-12/app.js
@@ -22,3 +22,27 @@
 // console.log("round off value: "+user_round );
 // console.log("floor value: "+ user_floor);
 // console.log("ceil value: "+ user_ceil   );
+
+// Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+// var user = +prompt("Enter a Negative Number")
+// if (user < 0) {
+//     user;
+//     console.log("Number: "+ user);
+// } else {
+//     console.log("Number: "+ user);
+//     var user = -1*user;
+//     console.log("after change into negative Number: "+ user);
+// }
+
+
+// var user_round = Math.round(user);
+// var user_floor = Math.floor(user);
+// var user_ceil = Math.ceil(user);
+// console.log("round off value: "+user_round );
+// console.log("floor value: "+ user_floor);
+// console.log("ceil value: "+ user_ceil   );
