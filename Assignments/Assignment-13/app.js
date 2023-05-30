334dffa26c943b9fbdf249813ca78203249b1565
maryam-develop <maryamdev.lop@gmail.com>
2023-05-31 01:50:04 +0500

    FEAT: qno 3

--- Assignments/Assignment-13/app.js
+++ Assignments/Assignment-13/app.js
@@ -7,8 +7,14 @@
 // 2. Write a program that alerts the current month in words.
 // For example December.
 var current = new Date()
-var month = current.getMonth()
-var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-console.log(monthName[month])
+// var month = current.getMonth()
+// var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// console.log(monthName[month])
 
+// 3. Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show
+// Sun.
 
+var day = current.getDay()
+var days=["Sunday","Saturday", "Monday", "Tuesday", "wednesday"," thursday","friday"]
+console.log(days[day]);
