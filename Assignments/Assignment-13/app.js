e736ce571e6620ef7eab168604f2afe910fe890a
maryam-develop <maryamdev.lop@gmail.com>
2023-05-31 01:56:10 +0500

    FEAT: qno 4

--- Assignments/Assignment-13/app.js
+++ Assignments/Assignment-13/app.js
@@ -6,7 +6,7 @@
 
 // 2. Write a program that alerts the current month in words.
 // For example December.
-var current = new Date()
+// var current = new Date()
 // var month = current.getMonth()
 // var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 // console.log(monthName[month])
@@ -14,7 +14,17 @@ var current = new Date()
 // 3. Write a program that alerts the first 3 letters of the current
 // day, for example if today is Sunday then alert will show
 // Sun.
+// var current = new Date()
+// var day = current.getDay()
+// var days=["Sunday","Saturday", "Monday", "Tuesday", "wednesday"," thursday","friday"]
+// console.log("Today is "days[day]);
 
-var day = current.getDay()
-var days=["Sunday","Saturday", "Monday", "Tuesday", "wednesday"," thursday","friday"]
-console.log(days[day]);
+// 4. Write a program that displays a message “It’s Fun day” if
+// its Saturday or Sunday today.
+// var current = new Date()
+// var day = current.getDay()
+// var days=["Sunday","Saturday", "Monday", "Tuesday", "wednesday"," thursday","friday"]
+// if (current == "Sunday" || current == "Saturday") {
+//     console.log("its a Funday");
+// }
+
