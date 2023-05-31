c696fa48aa77c50fca2fef48f639515c815d5145
maryam-develop <maryamdev.lop@gmail.com>
2023-05-31 14:29:33 +0500

    FEAT: qno 2

--- Assignments/Assignment-14/app.js
+++ Assignments/Assignment-14/app.js
@@ -7,3 +7,10 @@
 // }
 // CurrentDateTime();
 
+// 2. Write a function that takes first & last name and then it
+// greets the user using his full name
+// function greeting(first, last) {
+//     var fullname = first + " " + last;
+//     console.log("Assalam O alikum " + fullname);
+//     }
+//     greeting("Ibrahi","Adnan");
