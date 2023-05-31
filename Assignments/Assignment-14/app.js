14b37b547b41335f1c73620aa65d45a18edee332
maryam-develop <maryamdev.lop@gmail.com>
2023-05-31 14:27:49 +0500

    FEAT: qno 1

---
+++ Assignments/Assignment-14/app.js
@@ -0,0 +1,9 @@
+//  1 Write a function that displays current date & time in your
+// browser.
+
+// function CurrentDateTime() {
+//     var current = new Date();
+//     console.log(current);
+// }
+// CurrentDateTime();
+
