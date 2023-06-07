90b150ccbcdf69d6e07f5df69c5f14989fe53442
maryam-develop <maryamdev.lop@gmail.com>
2023-06-08 00:18:30 +0500

    qno1

---
+++ Assignments/Assignment-38-44/app.js
@@ -0,0 +1,10 @@
+// 1. Write a custom function power ( a, b ), to calculate the value of
+// a raised to b.
+// function power(a,b) {
+//     console.log(Math.pow(a,b))   
+// }
+// power(2,4)
+
+2. Any year is entered through the keyboard. Write a function to
+determine whether the year is a leap year or not.
+Leap years ..., 2012, 2016, 2020, ...
