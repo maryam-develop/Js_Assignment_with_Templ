6aaa559e69d029912ab259cfb12f8514f51fec89
maryam-develop <maryamdev.lop@gmail.com>
2023-06-07 06:58:26 +0500

    FEAT: qno 14

--- Assignments/Assignment-14/app.js
+++ Assignments/Assignment-14/app.js
@@ -197,3 +197,30 @@
 // }
 
 // document.write(countLetter("JSResourceS.com","o")); 
+
+// 14. The Geometrizer
+// Create 2 functions that calculate properties of a circle, using
+// the definitions here.
+// Create a function called calcCircumference:
+// • Pass the radius to the function.
+// • Calculate the circumference based on the radius, and output
+// "The circumference is NN".
+// Create a function called calcArea:
+// • Pass the radius to the function.
+// • Calculate the area based on the radius, and output "The area
+// is NN".
+// Circumference of circle = 2πr
+// Area of circle = πr2
+
+// document.write("<b>The Geometrizer</b><br>");
+// function calcCircumference(radius){
+//   var cuircum= 2* Math.PI*radius;
+// console.log("The circumference is "+cuircum);
+// }
+// calcCircumference(3)
+
+// function calcArea(radius){
+//     var area= Math.PI * radius**2;
+//   console.log("The Area is "+ area);
+//   } 
+// calcArea(2)
